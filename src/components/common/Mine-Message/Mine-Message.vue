3975f58286e87cb6c8a6cc411a3229fec7fb9d5f
ChenSan666 <chdccylj09@126.com>
2017-12-07 21:17:41 +0800

    Mine-Message

---
+++ src/components/common/Mine-Message/Mine-Message.vue
@@ -0,0 +1,88 @@
+<template>
+	<div class="mine-msg">
+		<div class="msg-content">
+			<div class="msg-wrapper">
+				<p class="user-name">儒家文化哈哈就</p>
+				<div class="user-msg-box">
+					<div class="triangle"></div>
+					<div class="message">sasdad asd送达  的阿斯达大大大大多ssadasdsada</div>
+				</div>
+			</div>
+		</div>
+		<div class="user-avatar">
+			<span class="user-avatar-name">u</span>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped lang="scss">
+@import "~@/common/scss/device-width.scss";
+.mine-msg {
+  display: flex;
+  .user-avatar {
+    flex: 0 0 32px;
+    width: 32px;
+    height: 32px;
+    border-radius: 16px;
+    line-height: 32px;
+    font-size: 16px;
+    text-align: center;
+    background-color: $user-color;
+  }
+  .msg-content {
+    // display: inline-block;
+    // vertical-align: top;
+    flex: 1;
+    padding-right: 8px;
+    box-sizing: border-box;
+    .msg-wrapper {
+      float: right;
+      // transform: rotateY(180deg);
+      text-align: right;
+      .user-name {
+        margin-bottom: 8px;
+        font-size: 12px;
+        color: $user-color;
+        overflow: hidden;
+      }
+      .user-msg-box {
+        position: relative;
+        display: inline-block;
+        max-width: 240px;
+        padding: 6px 10px 8px 10px;
+				border-radius: 4px;
+				text-align: left;
+        background-color: #fff;
+        word-wrap: break-word;
+        @media (min-width: $width-sm) {
+          max-width: 200px;
+        }
+        @media (min-width: $width-md) {
+          max-width: 250px;
+        }
+        @media (min-width: $width-desktop) {
+          max-width: 500px;
+        }
+        .triangle {
+					position: absolute;
+					right: -8px;
+					// margin-left: -18px;
+					// transform: rotate()
+          border: 4px solid transparent;
+          border-left-color: #fff;
+        }
+        .message {
+          // display: inline-block;
+					// transform: rotateY(180deg);
+					line-height: 24px;
+          word-wrap: break-word;
+        }
+      }
+    }
+  }
+}
+</style>
