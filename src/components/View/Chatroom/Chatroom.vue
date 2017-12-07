b08ca30fbb75b915deee85422837c9978e8334cd
ChenSan666 <chdccylj09@126.com>
2017-12-07 21:18:30 +0800

    update

--- src/components/View/Chatroom/Chatroom.vue
+++ src/components/View/Chatroom/Chatroom.vue
@@ -14,44 +14,13 @@
 		<div class="room-centent">
 			<ul class="message-list">
 				<li class="message-item">
-					<div class="sys-info">
-						<p class="log-out-in">zzz离开了群聊</p>	
-					</div>		
-				</li>
-				<li class="message-item">
-					<div class="sys-info">
-						<p class="log-out-in">2015/9/12 21:18:32</p>	
-					</div>		
+					<sys-info></sys-info>
 				</li>
 				<li class="message-item">	
-					<div class="user-msg">
-						<div class="user-avatar">
-							<span class="user-avatar-name">u</span>
-						</div>
-						<div class="msg-content">
-							<p class="user-name">asda</p>
-							<div class="user-msg-box">
-								<div class="triangle"></div>
-								<div class="message">asdasddddddddddd</div>
-							</div>
-						</div>
-					</div>
+					<user-message></user-message>
 				</li>
 				<li class="message-item">	
-					<div class="mine-msg">
-						<div class="msg-content">
-							<div class="msg-wrapper">
-								<p class="user-name">儒家文化哈哈就</p>
-								<div class="user-msg-box">
-									<div class="triangle"></div>
-									<div class="message">sssssssddddddddddddddddddssssssssssssssssssssssssssssdss</div>
-								</div>
-							</div>
-						</div>
-						<div class="user-avatar">
-							<span class="user-avatar-name">u</span>
-						</div>
-					</div>
+					<mine-message></mine-message>	
 				</li>
 			</ul>
 		</div>
@@ -64,6 +33,9 @@
 </template>
 
 <script>
+import SysInfo from "@/components/common/Sys-Info/Sys-Info";
+import UserMessage from "@/components/common/User-Message/User-Message";
+import MineMessage from "@/components/common/Mine-Message/Mine-Message";
 export default {
   name: "charroom",
   data() {
@@ -77,7 +49,12 @@ export default {
         }
       ]
     };
-  }
+  }, 
+	components:{
+		SysInfo,
+		UserMessage,
+		MineMessage
+	}
 };
 </script>
 
@@ -129,126 +106,6 @@ export default {
     .message-list {
       .message-item {
         margin: 12px 0;
-        .sys-info {
-          font-size: 10px;
-          font-weight: 100;
-          color: #ccc;
-          text-align: center;
-        }
-        .user-msg {
-          display: flex;
-          .user-avatar {
-            flex: 0 0 32px;
-            width: 32px;
-            height: 32px;
-            border-radius: 16px;
-            line-height: 32px;
-            font-size: 16px;
-            text-align: center;
-            background-color: #f4f;
-          }
-          .msg-content {
-            // display: inline-block;
-            // vertical-align: top;
-            flex: 1;
-            padding-left: 8px;
-            box-sizing: border-box;
-            .user-name {
-              margin-bottom: 8px;
-              font-size: 12px;
-              color: #ccc;
-              overflow: hidden;
-            }
-            .user-msg-box {
-              position: relative;
-              display: inline-block;
-              max-width: 240px;
-              padding: 6px 10px 8px 10px;
-              border-radius: 4px;
-              background-color: #fff;
-              word-wrap: break-word;
-              @media (min-width: $width-sm) {
-                max-width: 200px;
-              }
-              @media (min-width: $width-md) {
-                max-width: 250px;
-              }
-              @media (min-width: $width-desktop) {
-                max-width: 500px;
-              }
-              .triangle {
-                position: absolute;
-                margin-left: -18px;
-                border: 4px solid transparent;
-                border-right-color: #fff;
-              }
-              .message {
-                // display: inline-block;
-                word-wrap: break-word;
-              }
-            }
-          }
-        }
-        .mine-msg {
-          display: flex;
-          .user-avatar {
-            flex: 0 0 32px;
-            width: 32px;
-            height: 32px;
-            border-radius: 16px;
-            line-height: 32px;
-            font-size: 16px;
-            text-align: center;
-            background-color: $user-color;
-          }
-          .msg-content {
-            // display: inline-block;
-            // vertical-align: top;
-            flex: 1;
-            padding-right: 8px;
-            box-sizing: border-box;
-            .msg-wrapper {
-							float: right;
-							// transform: rotateY(180deg);
-							text-align: right;
-              .user-name {
-                margin-bottom: 8px;
-                font-size: 12px;
-                color: $user-color;
-                overflow: hidden;
-              }
-              .user-msg-box {
-                position: relative;
-                display: inline-block;
-                max-width: 240px;
-                padding: 6px 10px 8px 10px;
-                border-radius: 4px;
-                background-color: #fff;
-                word-wrap: break-word;
-                @media (min-width: $width-sm) {
-                  max-width: 200px;
-                }
-                @media (min-width: $width-md) {
-                  max-width: 250px;
-                }
-                @media (min-width: $width-desktop) {
-                  max-width: 500px;
-                }
-                .triangle {
-                  position: absolute;
-                  margin-left: -18px;
-                  border: 4px solid transparent;
-                  border-right-color: #fff;
-                }
-                .message {
-									// display: inline-block;
-									// transform: rotateY(180deg);
-                  word-wrap: break-word;
-                }
-              }
-            }
-          }
-        }
       }
     }
   }
