d4b74ab7ba461c999b265cc126c8ed23815d04bb
ChenSan666 <chdccylj09@126.com>
2017-12-07 21:18:59 +0800

    Sys-Info

---
+++ src/components/common/Sys-Info/Sys-Info.vue
@@ -0,0 +1,20 @@
+<template>
+	<div class="sys-info">
+		<p class="log-out-in">zzz离开了群聊</p>	
+	</div>
+</template>
+
+<script>
+	export default {
+		
+	}
+</script>
+
+<style scoped lang="scss">
+	.sys-info {
+		font-size: 10px;
+		font-weight: 100;
+		color: #ccc;
+		text-align: center;
+	}
+</style>
